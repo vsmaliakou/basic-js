d1d909140bde6f47e9f180a4edb503598ba8e48a
vsmaliakou <vsmaliakou@axmit.com>
2021-09-12 21:27:47 +0300

    feat: solve tasks carbon-dating, count cats, dream team, what season

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,13 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+  let out = [];
+  if(!Array.isArray(members)) return false;
+  for(let i = 0; i < members.length; i++){
+    if(typeof members[i] === 'string'){
+      out.push(members[i].trim()[0].toUpperCase())
+    }
+  }  
+  return out.length > 0 ? out.sort().join('') : false
 }
--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,37 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+  if(!date) return 'Unable to determine the time of year!';
+  if(!(date instanceof Date) || Object.keys(date).length > 0) throw new Error('Invalid date!');
+  let month = date.getMonth();
+  switch(month){
+    case 11:
+    case 0:
+    case 1:
+      return 'winter';
+    case 2:
+    case 3:
+    case 4:
+      return 'spring';
+    case 5:
+    case 6:
+    case 7:
+      return 'summer';
+    case 8:
+    case 9:
+    case 10:
+      return 'autumn';
+  }
 }
+  // let month = date.toLocaleString('en-US', {month: 'long'})
+  // if(month === 'January' || month === 'February' || month === 'December'){
+  //     return 'winter'
+  // } else if(month === 'March' || month === 'April' || month === 'May'){
+  //     return 'spring'
+  // } else if(month === 'June' || month === 'July' || month === 'August'){
+  //     return 'summer'
+  // } else {
+  //     return 'autumn'
+  // }
+
