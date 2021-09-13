cb48a73bdef3671c867ace104cae4a2c4c2b40c5
vsmaliakou <vsmaliakou@axmit.com>
2021-09-13 23:03:42 +0300

    feat: solve tasks hanoi-tower, recursive-depth, simple-chain, transform-array

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,25 +4,31 @@ import { NotImplementedError } from '../extensions/index.js';
  * Implement chainMaker object according to task description
  * 
  */
+
 export default {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.chain.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if(position < 1 || position > this.getLength() || typeof position !== 'number'){
+      this.chain = [];
+      throw new Error(`You can't remove incorrect link!`)      
+    }
+    this.chain.splice(position - 1, 1)
+    return this    
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let str = this.chain.join('~~');
+    this.chain = [];
+    return str;
   }
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,21 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if(!(arr instanceof Array)) throw new Error("'arr' parameter must be an instance of the Array!");
+  const out = [];
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] === '--discard-next'){
+      i++;
+    } else if(arr[i] === '--double-prev'){
+      arr[i - 2] !== '--discard-next' && out.push(arr[i - 1]);
+    } else if(arr[i] === '--double-next'){
+      out.push(arr[i + 1]);
+    } else if(arr[i] === '--discard-prev'){
+      arr[i - 2] !== '--discard-next' && out.pop();
+    } else {
+      out.push(arr[i])
+    }
+  }
+  return out.filter(a => a !== undefined);
 }
